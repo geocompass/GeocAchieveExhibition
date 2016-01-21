88ecd3e22705eea6086a05c88a0688db6ea787c8
wucangeo <wucangeo@gmail.com>
2016-01-21 11:53:57 +0800

    添加[重构数据]，待完善

--- app/app/directive/ngProject/ngProject.js
+++ app/app/directive/ngProject/ngProject.js
@@ -11,8 +11,9 @@ module.exports = function($rootScope, $http, FilterByKey) {
                 .success(function(_data) {
                     var data = _data;
                     if (data) {
-                        $scope.catagorySortby = "date"; //默认时间排序
-                        $scope.catagories = data.projects.data;
+                        $scope.catagorySort = "year"; //默认时间year排序，还可以类型type排序
+                        $scope.projects = data.projects;
+
                         // if ($scope.catagories) {
                         //     showProject();
                         // }
@@ -22,24 +23,35 @@ module.exports = function($rootScope, $http, FilterByKey) {
                 .error(function(data2) {
                     console.log("error");
                 });
-            // var showProject = function() {
-            //     var projects = $scope.projects;
-            //     console.log("projects:", $scope.projects);
-            // };
+
+            // 搜索过滤
+            $scope.searchProject = function() {
+                var result = FilterByKey.filterByKey($scope.projects, $scope.searchkey);
+                for (var p in result) {
+                    console.log(result[p].date, result[p].type, result[p].title);
+                }
+                console.log("-------------------");
+            };
 
             // 按照年份排序
-            $scope.sortByDate = function(data) {
-                var result = FilterByKey.sortByDate(data, $scope.searchKey);
-                for (var p in result) {
-                    console.log(result[p].date, result[p].type);
+            $scope.sortByDate = function() {
+                var result = FilterByKey.sortByAttr($scope.projects, ['date', -1], ['type', 1]);
+                $scope.catagorySort = "year"; //默认时间year排序，还可以类型type排序
+                $scope.catagories = FilterByKey.reconstructData(result, $scope.catagorySort);
+                for (var p in $scope.catagories) {
+                    console.log($scope.catagories[p].date, $scope.catagories[p].type);
                 }
+                console.log("-------------------");
             };
             // 按照类别排序
-            $scope.sortByType = function(data) {
-                var result = FilterByKey.sortByType(data, $scope.searchKey);
-                for (var p in result) {
-                    console.log(result[p].type, result[p].date);
+            $scope.sortByType = function() {
+                var result = FilterByKey.sortByAttr($scope.projects, ['type', 1], ['date', -1]);
+                $scope.catagorySort = "type"; //默认时间date排序，还可以类型type排序
+                $scope.catagories = FilterByKey.reconstructData(result, $scope.catagorySort);
+                for (var p in $scope.catagories) {
+                    console.log($scope.catagories[p].type, $scope.catagories[p].date);
                 }
+                console.log("-------------------");
             };
 
         }
--- app/app/service/filterbyKey.js
+++ app/app/service/filterbyKey.js
@@ -1,42 +1,92 @@
 module.exports = function($filter) {
-
-    var sortJsonArrayByProperty = function(objArray, sort1, sort2) {
+	// json 排序，支持两个属性
+    var sortJsonArray = function(objArray, sort1, sort2) {
         if (arguments.length < 2) throw new Error("sortJsonArrayByProp requires 2 arguments");
 
         if (objArray && objArray.constructor === Array) {
-            //第一个排序参数
+            // 第一个排序参数
             if (sort1.length != 2) {
                 return;
             }
-            var sort1Name = (sort1.constructor === Array) ? sort1[0] : ""; //1:ascending, 0:descending
-            var direct1 = (sort1.constructor === Array) && (sort1.length === 2) ? sort1[1] : 1; //1:ascending, 0:descending
-            //第二个排序参数
+            var sort1Name = (sort1.constructor === Array) ? sort1[0] : "";
+            var direct1 = (sort1.constructor === Array) && (sort1.length === 2) ? sort1[1] : 1; // 1:ascending, -1:descending
+            // 第二个排序参数
             var sort2Name = "";
             var direct2 = 1;
-
             if (arguments.length === 3) {
                 sort2Name = (sort1.constructor === Array) ? sort2[0] : "";
-                direct2 = (sort2.constructor === Array) && (sort2.length === 2) ? sort2[1] : 1; //1:ascending, 0:descending
+                direct2 = (sort2.constructor === Array) && (sort2.length === 2) ? sort2[1] : 1; // 1:ascending, -1:descending
             }
-            objArray.sort(function(a, b) {
+            // 排序
+            var result = objArray.sort(function(a, b) {
                 var comp = ((a[sort1Name] < b[sort1Name]) ? -1 * direct1 : ((a[sort1Name] > b[sort1Name]) ? 1 * direct1 : 0));
                 if (comp === 0 && sort2Name) {
                     comp = ((a[sort2Name] < b[sort2Name]) ? -1 * direct2 : ((a[sort2Name] > b[sort2Name]) ? 1 * direct2 : 0));
                 }
                 return comp;
             });
-            return objArray;
+            return result;
         }
     };
 
+    // json 过滤搜索
+    var filterJsonArray = function(objArray, filterKey) {
+        if (objArray && objArray.constructor === Array) {
+            if (filterKey) {
+                var result = objArray.filter(function(obj) {
+                    var result = obj.title.indexOf(filterKey) >= 0;
+                    return result;
+                });
+                console.log("filter:",result);
+                return result;
+            }
+        }
+    };
+
+    // 重新塑造结构
+    var reconstructData = function(data, catagory){
+    	var objArray = data;
+    	if (objArray && objArray.constructor === Array && catagory) {
+    		var result = [];
+    		var curCatagory = {};
+    		if(catagory === "date"){
+    			var curCtg = catagory;
+    			for(var obj in objArray){
+    				var prj = objArray[obj];
+    				if(curCtg != prj[catagory]){
+    					if(curCatagory){
+    						result.push(curCatagory);
+    					}
+    					curCatagory = {};
+    					curCatagory.name = prj.year;
+    					curCatagory.type = "year";
+    				}else{
+    					if(curCatagory){
+    						curCatagory.push(prj);
+    					}
+    				}
+    			}
+    		}else if(catagory === "type"){
+
+    		}
+    		return result;
+    	}
+    };
+
     return {
-        sortByDate: function(data, keyword) {
+    	// 排序
+        sortByAttr: function(data, sort1, sort2) {
             var results = data;
-            return sortJsonArrayByProperty(results, ['date', 0], ['type', 1]);
+            return sortJsonArray(results, sort1, sort2);
         },
-        sortByType: function(data, keyword) {
+        // 搜索
+        filterByKey: function(data, filterKey) {
             var results = data;
-            return sortJsonArrayByProperty(results, ['type', 1], ['date', 0]);
+            return filterJsonArray(results, filterKey);
         },
+        // 拼接结构
+        reconstructData: function(data,catagory){
+        	return reconstructData(data,catagory);
+        }
     };
 };
